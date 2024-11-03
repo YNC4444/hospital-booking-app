af0853c57c18d5ab2303bba850051cae64bfabf2
soysauce4444 <105949254+soysauce4444@users.noreply.github.com>
2024-11-03 14:56:14 -0500

    More realistic provider schedules

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -21,13 +21,24 @@ class ScheduleFactory extends Factory
     public function definition(): array
     {
         // Generate a random start and end time
-        $start_time = fake()->dateTimeBetween('07:00', '09:00')->format('H:i'); 
-        $end_time = fake()->dateTimeBetween('15:00', '17:00')->format('H:i'); 
+        $validStartTimes = ['07:00', '13:00', '15:00'];
+        $validEndTimes = ['13:00', '15:00', '20:00'];
+
+        // Randomly select a start time
+        $start_time = $this->faker->randomElement($validStartTimes);
+
+        // filter end times to ensure a minimum schedule of 5 hours
+        $filteredEndTimes = array_filter($validEndTimes, function ($end_time) use ($start_time) {
+            return strtotime($end_time) - strtotime($start_time) >= 5 * 60 * 60;
+        });
+
+        $end_time = $this->faker->randomElement($filteredEndTimes);
 
         return [
             // Create a new provider if not provided
             'provider_id' => Provider::factory(), 
-            'day_of_week' => fake()->randomElement(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']),
+            // 'day_of_week' => fake()->randomElement(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']),
+            'date' => $this->faker->date(),
             'start_time' => $start_time,
             'end_time' => $end_time,
             'status' => fake()->randomElement(['available', 'booked']),
---
+++ database/migrations/2024_11_03_194759_drop_dayoftheweek_add_date.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('schedules', function (Blueprint $table) {
+            $table->dropColumn('day_of_week');
+
+            $table->date('date')->after('provider_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('schedules', function (Blueprint $table) {
+            $table->string('day_of_week');
+
+            $table->dropColumn('date');
+        });
+    }
+};
