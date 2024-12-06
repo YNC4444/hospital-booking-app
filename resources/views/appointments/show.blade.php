ec99f8701e5c25909d5d4fc4dc136185ee1dadf1
soysauce4444 <105949254+soysauce4444@users.noreply.github.com>
2024-12-06 18:55:29 -0500

    appointment show

--- resources/views/appointments/show.blade.php
+++ resources/views/appointments/show.blade.php
@@ -2,87 +2,31 @@
 
 @section('content')
 <div class="container mx-auto px-4">
-  <h1 class="text-2xl font-bold mb-4">Add New Appointment</h1>
-
-  <!-- Display All Errors -->
-  @if ($errors->any())
-  <div class="alert alert-danger bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4">
-    <strong class="font-bold">Whoops! Something went wrong.</strong>
-    <ul class="mt-3 list-disc list-inside text-sm">
-      @foreach ($errors->all() as $error)
-      <li>{{ $error }}</li>
-      @endforeach
-    </ul>
+  <div class="flex justify-between items-center mb-4">
+    <h1 class="text-2xl font-bold">Appointment Details</h1>
+    <a href="{{ route('appointments.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      Back to Appointments
+    </a>
   </div>
-  @endif
-
-  <form action="{{ route('appointments.store') }}" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    @csrf
-    <div class="mb-4">
-      <label for="patient" class="block text-gray-700 text-sm font-bold mb-2">Patient</label>
-      <select name="patient_id" id="patient" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-        @foreach($patients as $patient)
-        <option value="{{ $patient->id }}">{{ $patient->fname }} {{ $patient->lname }}</option>
-        @endforeach
-      </select>
-      @error('patient')
-      <span class="text-red-500 text-sm">{{ $message }}</span>
-      @enderror
+    <div class="bg-white shadow-md rounded-lg p-4 mb-4 ">
+      <h2 class="text-xl font-bold">Provider: Dr.{{ $appointment->provider->lname }}</h2>
+      @if($appointment->patient)
+      <p class="text-gray-700">Patient: {{ $appointment->patient->fname }} {{ $appointment->patient->lname }}</p>
+      @else
+      <p class="text-gray-700">Patient: Unbooked</p>
+      @endif
+      <p class="text-gray-700">Service: {{ $appointment->service->name }}</p>
+      <p class="text-gray-700">Date: {{ $appointment->date }}</p>
+      <p class="text-gray-700">Time: {{ $appointment->start_time }} - {{ $appointment->end_time }}</p>
     </div>
-    <div class="mb-4">
-      <label for="date" class="block text-gray-700 text-sm font-bold mb-2">Date</label>
-      <!-- select date with only future dates possible -->
-      <input type="date" name="date" id="date" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value="{{ now()->toDateString() }}" min="{{ now()->toDateString() }}" required>
-      @error('date')
-      <span class="text-red-500 text-sm">{{ $message }}</span>
-      @enderror
-    </div>
-    <div class="mb-4">
-      <label for="schedule" class="block text-gray-700 text-sm font-bold mb-2">Schedule</label>
-      <select name="schedule_id" id="schedule" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-        <!-- Options will be dynamically loaded based on the selected date -->
-      </select>
-      @error('schedule_id')
-      <span class="text-red-500 text-sm">{{ $message }}</span>
-      @enderror
-    </div>
-    <div class="mb-4">
-      <label for="service" class="block text-gray-700 text-sm font-bold mb-2">Service</label>
-      <select name="service_id" id="service" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-        <!-- @foreach($services as $service)
-        <option value="{{ $service->id }}">{{ $service->name }}</option>
-        @endforeach -->
-        <!-- Options will be dynamically loaded based on the selected schedule and provider -->
-      </select>
-      @error('service_id')
-      <span class="text-red-500 text-sm">{{ $message }}</span>
-      @enderror
-    </div>
-    <div class="mb-4">
-      <label for="start_time" class="block text-gray-700 text-sm font-bold mb-2">Start Time</label>
-      <select name="start_time" id="start_time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-        <!-- Options will be dynamically loaded based on the selected schedule -->
-      </select>
-      @error('start_time')
-      <span class="text-red-500 text-sm">{{ $message }}</span>
-      @enderror
-    </div>
-    <div class="mb-4">
-      <label for="end_time" class="block text-gray-700 text-sm font-bold mb-2">End Time</label>
-      <select name="end_time" id="end_time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-        <!-- Options will be dynamically loaded based on the selected schedule -->
-      </select>
-      @error('end_time')
-      <span class="text-red-500 text-sm">{{ $message }}</span>
-      @enderror
-    </div>
-
     <div class="flex items-center justify-between">
-      <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        Add Appointment
-      </button>
+      <a href="{{ route('appointments.edit', $appointment->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        Edit
+      </a>
+      <a href="{{ route('appointments.trash', $appointment->id) }}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        Delete
+      </a>
     </div>
-  </form>
 </div>
 
 @endsection
