b137bd66ac289dd82e2663ae35ec02e9eb6c1c8b
soysauce4444 <105949254+soysauce4444@users.noreply.github.com>
2024-12-09 22:06:03 -0500

    add breeze options selected: blade, dark mode, PHPUnit

--- routes/web.php
+++ routes/web.php
@@ -1,103 +1,20 @@
 <?php
 
-// use App\Models\Patient;
-
-use App\Http\Controllers\AppointmentController;
-use App\Http\Controllers\PatientController;
-use App\Http\Controllers\ProviderController;
-use App\Http\Controllers\ScheduleController;
-use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// ---------------patients
-Route::get(
-  'patients/trash/{id}',
-  [PatientController::class, 'trash'])
-  ->name('patients.trash');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get(
-  'patients/trashed',
-  [PatientController::class, 'trashed'])
-  ->name('patients.trashed');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get(
-  'patients/restore/{id}',
-  [PatientController::class, 'trash'])
-  ->name('patients.restore');
-
-// -------------providers
-Route::get(
-  'providers/trash/{id}',
-  [ProviderController::class, 'trash'])
-  ->name('providers.trash');
-
-Route::get(
-  'providers/trashed',
-  [ProviderController::class, 'trashed'])
-  ->name('providers.trashed');
-
-Route::get(
-  'providers/restore/{id}',
-  [ProviderController::class, 'trash'])
-  ->name('providers.restore');
-
-// -------------- schedules
-Route::get(
-  'schedules/trash/{id}',
-  [ScheduleController::class, 'trash'])
-  ->name('schedules.trash');
-
-Route::get(
-  'schedules/trashed',
-  [ScheduleController::class, 'trashed'])
-  ->name('schedules.trashed');
-
-Route::get(
-  'schedules/restore/{id}',
-  [ScheduleController::class, 'trash'])
-  ->name('schedules.restore');
-
-// -------------- appointments
-Route::get(
-  'appointments/trash/{id}',
-  [AppointmentController::class, 'trash'])
-  ->name('appointments.trash');
-
-Route::get(
-  'appointments/trashed',
-  [AppointmentController::class, 'trashed'])
-  ->name('appointments.trashed');
-
-Route::get(
-  'appointments/restore/{id}',
-  [AppointmentController::class, 'trash'])
-  ->name('appointments.restore');
-
-// -------------- api endpoints
-Route::get(
-  '/api/schedules', 
-  [ScheduleController::class, 'getSchedulesByDate']);
-
-Route::get(
-  '/api/schedules/{schedule}/times', 
-  [ScheduleController::class, 'getAvailableTimes']);
-
-Route::get(
-  '/api/schedules/{schedule}/services',
-  [ScheduleController::class, 'getServices']);
-
-Route::get(
-  '/api/providers/{provider}/services',
-  [ProviderController::class, 'getServices']);
-
-Route::resource('patients', PatientController::class);
-Route::resource('providers', ProviderController::class);
-Route::resource('services', ServiceController::class);
-Route::resource('schedules', ScheduleController::class);
-Route::resource('appointments', AppointmentController::class);
-
-
+require __DIR__.'/auth.php';
